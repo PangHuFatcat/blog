704f85851bacddb7df93b6a8a27b8169b62beb6d
周广俊 <743137213@qq.com>
2021-08-26 09:46:38 +0800

    feat 去掉不需要的

---
+++ pages/about/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next'
+import styles from './index.module.css'
+
+const About: NextPage = () => {
+  return (
+    <div className={styles.about}>about</div>
+  )
+}
+
+export default About
