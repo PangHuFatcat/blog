833b6833144edbcea162507001dab9fb5f116db0
周广俊 <743137213@qq.com>
2021-09-04 11:24:55 +0800

    feat title

--- components/PageLayout/index.tsx
+++ components/PageLayout/index.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { PropsWithChildren } from "react";
+import Head from "next/head";
 import styles from "./index.module.css";
 
-interface Props {}
+interface Props {
+  title?: string;
+}
 
-const PageLayout: React.FC<Props> = (props) => {
-  const { children } = props;
-  return <div className={styles.pageLayout}>{children}</div>;
+const PageLayout = (props: PropsWithChildren<Props>) => {
+  const { children, title = 'PangHu' } = props;
+  return (
+    <div className={styles.pageLayout}>
+      <Head>
+        <title>{title}</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
+      </Head>
+      {children}
+    </div>
+  );
 };
 
 export default React.memo(PageLayout);
