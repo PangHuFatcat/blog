a1e7c0fb082a9e1eeddf1a07988848bf4221953f
周广俊 <743137213@qq.com>
2021-09-04 17:45:03 +0800

    feat 域名

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
-
 const isProd = process.env.NODE_ENV === 'production'
 
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-  assetPrefix: isProd ? 'https://blog-kappa-two.vercel.app' : ''
+    reactStrictMode: true,
+    assetPrefix: isProd ? 'https://panghu.dev' : '',
 }
