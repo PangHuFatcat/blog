747470bb2fefc3cc5d8aa0a0a5086203dd282e65
周广俊 <743137213@qq.com>
2021-09-12 19:10:13 +0800

    feat mongodb

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,32 +1,31 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient, MongoClientOptions } from 'mongodb'
 
-const uri = process.env.MONGODB_URI
-const options = {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
+class DB {
+    static client: MongoClient
+
+    static clientConnect: Promise<MongoClient>
+
+    constructor() {}
+
+    static init = () => {
+        if (!process.env.MONGODB_URI) {
+            throw new Error('似乎在 .env 里面没有配置 MONGODB_URI')
+        }
+
+        const options: MongoClientOptions = {}
+
+        if (!DB.client) {
+            console.log('没有DB.client: 创建')
+            DB.client = new MongoClient(process.env.MONGODB_URI, options)
+        }
+
+        if (!DB.clientConnect) {
+            console.log('DB.clientConnect: 创建')
+            DB.clientConnect = DB.client.connect()
+        }
+
+        return DB.clientConnect
+    }
 }
 
-let client
-let clientPromise
-
-if (!uri) {
-  throw new Error('Please add your Mongo URI to .env.local')
-}
-
-if (process.env.NODE_ENV === 'development') {
-  // In development mode, use a global variable so that the value
-  // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options)
-    global._mongoClientPromise = client.connect()
-  }
-  clientPromise = global._mongoClientPromise
-} else {
-  // In production mode, it's best to not use a global variable.
-  client = new MongoClient(uri, options)
-  clientPromise = client.connect()
-}
-
-// Export a module-scoped MongoClient promise. By doing this in a
-// separate module, the client can be shared across functions.
-export default clientPromise
+export default DB
