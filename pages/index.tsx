b4feb562d7d82dccce85d7f0c2ce8018d508081f
周广俊 <743137213@qq.com>
2021-09-18 15:14:25 +0800

    feat mongo find

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,112 +1,35 @@
 import type { GetServerSideProps, NextPage } from 'next'
-import Image from 'next/image'
 import PageLayout from '../components/PageLayout'
 import WidgetLayout from '../components/WidgetLayout'
-import IconFont from '../components/IconFont'
-import { PhotoProvider, PhotoConsumer } from 'react-photo-view'
 import mongodb from '../lib/mongodb'
 import styles from './index.module.scss'
 
+interface Post {
+    title: string
+    content: string
+}
 interface IndexProps {
-    [key: string]: any
+    posts: Post[]
 }
 
 const Index: NextPage<IndexProps> = (props) => {
-    const photoImages = [
-        '/images/test/1.jpg',
-        '/images/test/2.jpg',
-        '/images/test/3.jpg',
-        '/images/test/4.jpg',
-        '/images/test/5.png',
-        '/images/test/6.jpg',
-        '/images/test/7.png',
-        '/images/test/8.jpg',
-        '/images/test/9.jpg',
-    ]
+    const { posts } = props
     return (
         <PageLayout>
             <div className={styles.index}>
-                <div className={styles.tool}>
-                    <WidgetLayout styleType="capsule" className={styles.toolItem}>
-                        <IconFont name="search" size={20} color="#444"></IconFont>
-                        <span>搜索</span>
-                    </WidgetLayout>
-                    <WidgetLayout styleType="capsule" className={styles.toolItem}>
-                        <IconFont name="document" size={20} color="#444"></IconFont>
-                        <span>简历</span>
-                    </WidgetLayout>
-                    <WidgetLayout styleType="capsule" className={styles.toolItem}>
-                        <IconFont name="picture" size={20} color="#444"></IconFont>
-                        <span>相册</span>
-                    </WidgetLayout>
-                    <WidgetLayout styleType="capsule" className={styles.toolItem}>
-                        <IconFont name="system" size={20} color="#444"></IconFont>
-                        <span>设置</span>
-                    </WidgetLayout>
-                </div>
-                <div className={styles.post}>
-                    <WidgetLayout styleType="card">
-                        <div>
-                            <h1>从 iOS 14 到 Android 12，桌面小组件是怎么「文艺复兴」的</h1>
-                            <p>
-                                时尚无关，但这里要提啊伟大伟大伟大到的存 阿瓦达作系统选择，在新
-                                啊伟大伟大啊吴大维
-                                本文的主题虽然与时尚无关，但这里要提啊伟大伟大伟大到的存在，同样遵循着这一定律：桌面小组件在智能手机出现早期便已随之诞生，但直到智能手机操作系统高度成熟的
-                                2020 年，重新被包括苹果 iOS
-                                在内的主流操作系统选择，在新的需求下，重新作为手机系统更新的重要功能面向新的用户。
-                            </p>
-                            <div className={styles.photo}>
-                                <PhotoProvider>
-                                    {photoImages.map((item, index) => (
-                                        <PhotoConsumer key={index} src={item} intro={item}>
-                                            <div
-                                                style={{
-                                                    width: '100%',
-                                                    height: '100%',
-                                                    display: 'grid',
-                                                }}
-                                            >
-                                                <Image
-                                                    className={styles.photoImage}
-                                                    src={item}
-                                                    alt=""
-                                                    width={200}
-                                                    height={200}
-                                                    objectFit="cover"
-                                                ></Image>
-                                            </div>
-                                        </PhotoConsumer>
-                                    ))}
-                                </PhotoProvider>
+                {posts.map((item, index) => (
+                    <div className={styles.post} key={index}>
+                        <WidgetLayout styleType="card">
+                            <div>
+                                <h1>{item.title}</h1>
+                                <p>{item.content}</p>
                             </div>
-                            <p>What about the next?</p>
-                            <p>is this real?</p>
-                        </div>
-                    </WidgetLayout>
-                </div>
-                <div className={styles.post}>
-                    <WidgetLayout styleType="card">
-                        <div>
-                            <h1>完全是测一下换行而已</h1>
-                            <p>
-                                时尚界一直以来有一个著名的理论：在某一时代流行的时尚元素，在经过一段时间的沉寂之后，会被人们再次拿出来利用。
-                                这便是「弗莱定律」，它解释了为什么在长期的历史中，为什么很多曾经时尚的设计会随着设计审美的变化变过时，又随着时间的推移逐渐被人记起，重新焕发生机。
-                                本文的主题虽然与时尚无关，但这里要提到的存在，同样遵循着这一定律：桌面小组件在智能手机出现早期便已随之诞生，但直到智能手机操作系统高度成熟的
-                                2020 年，重新被包括苹果 iOS
-                                在内的主流操作系统选择，在新的需求下，重新作为手机系统更新的重要功能面向新的用户。
-                            </p>
-                        </div>
-                    </WidgetLayout>
-                </div>
-                <div className={styles.simpleEmpty}>暂无更多~</div>
+                        </WidgetLayout>
+                    </div>
+                ))}
             </div>
         </PageLayout>
     )
-}
-
-interface Haiku {
-    title: string
-    content: string
 }
 
 export const getServerSideProps: GetServerSideProps<IndexProps> = async () => {
@@ -114,18 +37,13 @@ export const getServerSideProps: GetServerSideProps<IndexProps> = async () => {
 
     const database = client.db('test_db')
 
-    const haiku = database.collection<Haiku>('haiku')
+    const posts = database.collection<Post>('post')
 
-    const result = await haiku.insertOne({
-        title: 'Record of a Shriveled Datum',
-        content: 'No bytes, no problem. Just insert a document, in MongoDB',
-    })
-
-    console.log(result, 'result')
+    const postArray = await posts.find<Post>({}, { projection: { _id: 0 } }).toArray()
 
     return {
         props: {
-            // result,
+            posts: postArray,
         },
     }
 }
